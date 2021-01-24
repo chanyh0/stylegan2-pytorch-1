f96fcf6d17e881ff8479d1e3e18113ac5dcf6232
Ch4nYH <hfercxx@hotmail.com>
2021-01-25 00:34:08 +0800

    update

--- stylegan2_pytorch/stylegan2_pytorch.py
+++ stylegan2_pytorch/stylegan2_pytorch.py
@@ -1368,7 +1368,7 @@ class ModelLoader:
 
 
 
-def PGD(x, q_loss, loss, model=None, steps=1, gamma=0.001):
+def PGD(x, q_loss, loss, model=None, steps=1, gamma=0.003):
     
     # Compute loss
     x_adv = x.clone()
@@ -1381,7 +1381,7 @@ def PGD(x, q_loss, loss, model=None, steps=1, gamma=0.001):
 
     return x_adv
 
-def PGD_G(x, style, input_noise, gen_model, dis_model, steps=1, gamma=0.001, eps=(1/255), randinit=False, clip=False):
+def PGD_G(x, style, input_noise, gen_model, dis_model, steps=1, gamma=0.003, eps=(1/255), randinit=False, clip=False):
     
     # Compute loss
     x_adv = x.clone()
